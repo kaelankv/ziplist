83760af662fc86c6760f3712dff64d90bdae6154
kakokapolei <95083722+kakokapolei@users.noreply.github.com>
2024-02-05 19:42:29 -1000

    Finished in 8 min

---
+++ ziplist.js
@@ -0,0 +1,16 @@
+const list1 = ['a', 'b', 'c'];
+const list2 = [1, 2, 3];
+
+function zipList(first, second) {
+  const list = [];
+  for (let i = 0; i < first.length; i++) {
+    list.push(first[i], second[i]);
+  }
+  return list;
+}
+
+console.log(zipList(list1, list2));
+
+const zipListTheSimpleWay = (first, second) => _.flatten(_.zip(first, second));
+
+console.log(zipListTheSimpleWay(list1, list2));
